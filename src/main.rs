d2a4037af1c98109b5c60c21433159aafb56ca00
dcbuilder.eth <dcbuilder@protonmail.com>
2023-04-11 16:00:45 -0400

    better(headers)

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,36 @@ use clipboard::{ClipboardContext, ClipboardProvider};
 fn main() {
     let input = env::args().collect::<Vec<String>>()[1..].join(" ");
 
+    let slashes = String::from("    ///////////////////////////////////////////////////////////////////");
+
+    let slashes_len = slashes.len();
+
+    let mut space_start = String::from("");
+
+    while (61 - (space_start.len() + input.len())) > space_start.len() {
+        space_start.push_str(" ")
+    }
+
+    let space_len = space_start.len();
+    
+    let space_end_len = slashes_len - 10 - space_len - input.len();
+    
+    let mut space_end = String::from("");
+
+    for _ in 0..space_end_len {
+        space_end.push_str(" ")
+    }
+
     let output = format!(
-        "{}\n{}{}{}\n{}",
-        "    /*//////////////////////////////////////////////////////////////",
+        "{}\n{}{}{}{}{}{}\n{}",
+        slashes,
         "    ",
-        (0..(64 - input.len()) / 2).map(|_| " ").collect::<String>(),
+        "///",
+        space_start,
         input.to_uppercase(),
-        "    //////////////////////////////////////////////////////////////*/"
+        space_end,
+        "///",
+        slashes
     );
 
     println!("{}", output); // Print the header to console.
